be9db530d33188fdbcd82af203ea626984da4295
benjamin gonzalez <jgonzalez.benjamin@gmail.com>
2024-02-16 13:01:28 -0300

    hola mundo nextjs

---
+++ app/pages/_document.js
@@ -0,0 +1,19 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {    
+    return (
+        <Html>
+            <Head>
+                <meta name='description' content='GuitarLA - Sale guitars and a music blog' />
+                <link rel="preconnect" href="https://fonts.googleapis.com" />
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+                <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@100..900&display=swap" rel="stylesheet" />
+                <link rel="stylesheet" type='css' href='https://necolas.github.io/normalize.css/8.0.1/normalize.css' />
+            </Head>
+            <body>
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    );
+}
